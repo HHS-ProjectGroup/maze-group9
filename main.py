c20d4d66880bf844c9319280dede932f2d1c7258
Selim Yaman <selim.yaman@proton.me>
2025-09-08 20:02:09 +0200

    initial commit on origin

---
+++ main.py
@@ -0,0 +1,52 @@
+# -----------------------------------------------------------------------------
+# File: main.py
+# ACS School Project - Simple Maze Example
+# Organization: THUAS (The Hague University of Applied Sciences)
+# Location: Delft
+# Date: July 2025
+# -----------------------------------------------------------------------------
+print("main started")
+from rooms import enterCorridor, enterStudyLandscape, enterClassroom2015, enterProjectRoom3
+print("main imports are resolved")
+
+print("****************************************************************************")
+print("*                      Welcome to the School Maze!                         *")
+print("*        Your goal is to explore all important rooms in the school.        *")
+print("*    You may need to solve challenges to collect items and unlock rooms.   *")
+print("*               Once you've visited all rooms, you win!                    *")
+print("****************************************************************************")
+
+state = { # this is a dictionary
+    "current_room": "corridor",
+    "previous_room": "corridor",
+    # add the visited rooms here
+    "visited": { # this is also a dictionary, i.e nested dict
+        "classroom2015": False,
+        "projectroom3": False,
+    },
+    "inventory": []
+    # In Python, [] defines an empty list. Python does not have an array type
+    # A list is an ordered, mutable container that can hold elements of any type(mixed types)
+}
+
+while True:
+    print("state started")
+    current = state["current_room"]
+
+    if current == "corridor":
+        state["current_room"] = enterCorridor(state)
+
+    elif current == "studylandscape":
+        state["current_room"] = enterStudyLandscape(state)
+
+    elif current == "classroom2015":
+        state["current_room"] = enterClassroom2015(state)
+
+    elif current == "projectroom3":
+        state["current_room"] = enterProjectRoom3(state)
+
+    else:
+        print("Unknown room. Exiting game.")
+        break
+
+print("main is run")
---
+++ rooms/__init__.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------------------------------------
+# File: __init__.py
+# ACS School Project - Simple Maze Example
+# Organization: THUAS (The Hague University of Applied Sciences)
+# Location: Delft
+# Date: July 2025
+# -----------------------------------------------------------------------------
+
+# Without this file, Python won't recognize a directory as a package.
+# Runs first when this package is imported
+print("init file started")
+from .corridor import enterCorridor
+from .studylandscape import enterStudyLandscape
+from .classroom2015 import enterClassroom2015
+from .projectroom3 import enterProjectRoom3
+from .utils import chooseNextRoom
+
+print("init file is run")
